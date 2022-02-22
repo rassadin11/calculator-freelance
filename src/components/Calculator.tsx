39ed3372784503b39e410dd7b52851b3dccb06e7
rassadin11 <artem.rassadin.05@mail.ru>
2022-02-22 19:17:11 +0300

    fix

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -31,10 +31,6 @@ const Calculator = (): JSX.Element => {
   const [signs, setSigns] = useState<string[]>([])
   const [temporaryResult, setTemporaryResult] = useState<string>('0')
 
-  React.useEffect(() => {
-    console.log(temporaryResult, signs, numbers)
-  }, [temporaryResult, signs])
-
   // start calculation
   const computeOperation = () => {
     setTemporaryResult(operate(numbers, signs, temporaryResult, setNumbers))
